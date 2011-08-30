57e5828f37a9542f76b61c53db6062e94af97003
amseledka <amseledka@gmail.com>
2011-08-30 10:05:47 +0300

    server side api request

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   def to_s
     "id: #{user_id} access_token: #{access_token}"
   end
+
+=begin  def get_auth
+    params = call_vk_method('auth')
+    update_attributes(:access_token => params[:access_token])
+  end
+=end
+  
+  def post_to_wall(wall_text, wall_attachment)
+    call_vk_method('post_to_wall', "post_text=#{wall_text}&attachment=#{wall_attachment}")
+  end
+
+protected
+  def call_vk_method(method, params)
+    response = open("https://api.vkontakte.ru/method/#{method}?#{params}&access_token=#{access_token}")
+    ActiveSupport::JSON.parse(response)
+  end
 end
