1a013c4e5c42b890af4056a33d13eb06e8a1edb2
amseledka <amseledka@gmail.com>
2011-09-03 14:49:48 +0300

    omniauth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 require 'omniauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :vkontakte, '2451953 ', 'wall', 'popup', 'http://api.vkontakte.ru/blank.html'
+  provider :vkontakte, '2451953 ', 'qTn9hZG52ixvgfaFUqGq'
 end
